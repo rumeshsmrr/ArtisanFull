f9481107e5e25bd662c26a0bbea05264bdb101d2
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-12-27 21:00:48 +0530

    new landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primaryFont: ["Olivera", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
